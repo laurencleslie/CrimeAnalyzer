dbf0a5dd4773a4b49c8b6db3924d9aa5c119cab1
laurencleslie <42555185+laurencleslie@users.noreply.github.com>
2018-11-28 13:42:08 -0600

    Add files via upload

---
+++ CrimeAnalyzer/CrimeData.cs
@@ -0,0 +1,27 @@
+namespace ConsoleApp
+
+{
+
+    public class CrimeData
+
+    {
+
+        public int Year { get; set; }
+
+        public int Robberies { get; set; }
+
+        public int ViolentCrimes { get; set; }
+
+        public int Murders { get; set; }
+
+        public int Population { get; set; }
+
+        public int Rapes { get; set; }
+
+        public int Thefts { get; set; }
+
+        public int MotorVehicleThefts { get; set; }
+
+    }
+
+}
